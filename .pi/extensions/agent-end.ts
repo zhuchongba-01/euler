59dfc2caf330c032d4ca7ed47e457c2dbd6af4cd
forkkk <243270432+zhuchongba-01@users.noreply.github.com>
2026-02-12 22:56:33 +0100

    Cleanup

--- .pi/extensions/agent-end.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-
-export default function agentEndExtension(pi: ExtensionAPI): void {
-	pi.on("agent_end", async (_event, ctx) => {
-		ctx.ui.notify("agent_end received");
-	});
-}
